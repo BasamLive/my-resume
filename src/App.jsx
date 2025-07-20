9b1bfd5e69046dd20b8fe4bb6c6a61c459ed906c
basam <basam.najwa11@gmail.com>
2025-07-20 22:43:07 +0400

    initial commit for building my resume

---
+++ src/App.jsx
@@ -0,0 +1,236 @@
+import { useRef, useEffect } from "react";
+import { useReactToPrint } from "react-to-print";
+
+import { IoLogoReact } from "react-icons/io5";
+import { MdAttachEmail } from "react-icons/md";
+import { MdPhoneCallback } from "react-icons/md";
+import { ImLocation2 } from "react-icons/im";
+import image from "./assets/basam.jpeg";
+import { FaBluesky } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub } from "react-icons/fa6";
+import { SiLichess } from "react-icons/si";
+
+const skills = [
+  { name: "HTML5", color: "#e34c26" }, // foundational and strong
+  { name: "CSS3", color: "#264de4" }, // creative styling wizard
+  { name: "JavaScript (ES6+)", color: "#f7df1e" }, // logic master, very capable
+  { name: "TypeScript", color: "#3178c6" }, // newer layer, but launching fast
+  { name: "ReactJS", color: "#61dafb" }, // your power tool, real passion
+  { name: "Nodejs", color: "#0f3" },
+  { name: "Redux", color: "#De73EE" },
+  { name: "Axios", color: "#80F" },
+  { name: "Git", color: "#Fa7970" },
+  { name: "SASS", color: "#cf649a" }, // clever with structure and nesting
+];
+
+const App = () => {
+  const contentRef = useRef(null);
+  const handlePrint = useReactToPrint({ contentRef });
+  useEffect(() => {
+    console.log("content ref:", contentRef.current);
+  }, []);
+  return (
+    <>
+      <div ref={contentRef} className="container">
+        <p className="date-details">
+          {" "}
+          print date {new Date().toLocaleDateString()}
+        </p>
+        <button onClick={handlePrint} className="print-btn">
+          Print
+        </button>
+        <img src={image} alt="basam's photo" className="picture" />
+        <div className="left-column">
+          <h1 className="name">basam mohamed</h1>
+          <h3 className="title">
+            <IoLogoReact style={{ fontSize: "16", paddingRight: "3px" }} />
+            React.Js Developer
+          </h3>
+          <p className="email">
+            <MdAttachEmail
+              style={{
+                fontSize: "16px",
+                marginTop: "4px",
+                paddingRight: "2px",
+              }}
+            />
+            <a href="mailto:basabasam@hotmail.com">basabasam@hotmail.com</a>
+          </p>
+          <p className="phone">
+            <MdPhoneCallback
+              style={{ marginTop: "3px", paddingRight: "2px" }}
+            />
+            <a href="tel:+971557657992">(+971)-557657992</a>
+          </p>
+          <p className="location">
+            <ImLocation2 /> Dubai UAE
+          </p>
+
+          <h2 className="skills-title">KEY SKILLS</h2>
+
+          <ul className="skills-container">
+            {skills.map((skill) => {
+              return (
+                <li
+                  className="skill"
+                  key={skill.name}
+                  style={{
+                    background: skill.color,
+                  }}
+                >
+                  <span>{skill.name}</span>
+                </li>
+              );
+            })}
+          </ul>
+          <div>
+            <h2>Soft Skills / Traits</h2>
+            <ul className="softskills-ul">
+              <li>Detail-Orinted </li>
+              <li>Resource Debugger </li>
+              <li>Problem Solver </li>
+              <li>Visual Thinker </li>
+              <li>Entrepreneurial Minder</li>
+              <li>Active Listener</li>
+              <li>Cross-cultural Sensitivity</li>
+              <li>Fast Learner</li>
+              <li>UI/UX Awareness</li>
+              <li>Analytical Thinker</li>
+            </ul>
+          </div>
+
+          <div className="languages">
+            <h2>Languages i speak</h2>
+            <div className="two-languages">
+              <p> [Arabic]</p>
+              <p>[English] </p>
+            </div>
+          </div>
+        </div>
+        <div className="right-column">
+          <section>
+            <h1 className="overview">Overview</h1>
+            <p className="overview-text">
+              I’m a frontend developer with a passion for crafting responsive,
+              scalable, and visually engaging web applications using ReactJS.
+              With a solid foundation in HTML, CSS, and JavaScript (ES6+), I
+              specialize in turning complex ideas into intuitive user interfaces
+              that balance functionality and design.
+            </p>
+          </section>
+          <section>
+            <h1 className="experience">Experience</h1>
+            <div>
+              <h2 className="experience-heading">Web Developer - Freeelance</h2>
+              <p className="experience-description">Remote / Jan 2024</p>
+              <h3 className="experience-highlight">Highlights:</h3>
+              <ul className="experience-list">
+                <li>
+                  Built a modular e-cmmerce web application using ReactJs,
+                  showcasing building materials across distinct categories
+                  (plumbing, Electrical, paint Cement) with dynamic product
+                  components.
+                </li>
+                <li>
+                  Designed and implemented a custom cart management system using
+                  Contex API, supporting item quantities pricing logic and state
+                  persistance.
+                </li>
+                <li>
+                  Engineered a payment confirmation workflow via uploads and
+                  manual transactions proof collection tailored to Sudan's
+                  banking limitations.
+                </li>
+              </ul>
+            </div>
+            <div>
+              <h2 className="experience-heading">Self-Initiated Portfolio</h2>
+              <p className="experience-description">
+                Personal Learning / 2024 2025
+              </p>
+              <h3 className="experience-highlight">Highlights:</h3>
+              <ul className="experience-list">
+                <li>
+                  Architected reusable React Components for resume pages and
+                  portfolio showcase
+                </li>
+                <li>
+                  Enhanced design with advanced typography customization, grid
+                  layouts and dynamic styling principles
+                </li>
+                <li>
+                  Demonstrated resourceful problem-solving by debugging
+                  system-level issues and optimizing performance across devices
+                </li>
+              </ul>
+            </div>
+          </section>
+
+          <section>
+            <div>
+              <h1 className="education-primary-heading">Education</h1>
+              <h3 className="education-secondary-heading">
+                Library & Information Science
+              </h3>
+              <p className="education-university-name">
+                Neelain University - Khartoum Sudan / 2009 - 2013
+              </p>
+            </div>
+            <div>
+              <h3 className="education-secondary-heading">
+                Web Developer Career Path
+              </h3>
+              <p className="education-university-name">
+                Scrimba Coding Platform - Online / 2024 - 2025
+              </p>
+            </div>
+            <div>
+              <h3 className="education-secondary-heading">Fronend Developer</h3>
+              <p className="education-university-name">
+                FreeCodeCamp Coding platform - Online / 2024 - 2025
+              </p>
+            </div>
+            <div>
+              <h3 className="education-secondary-heading">
+                Technical Support Engineer
+              </h3>
+              <p className="education-university-name">
+                Coursera Coding Platform - Online / 2024 - current
+              </p>
+            </div>
+            {/* <div>
+              <h3 className="education-secondary-heading">
+                Online Courses From
+              </h3>
+              <p className="education-university-name">
+                Brad Traversy , Jonas Schmedttman & Bob Ziroll
+              </p>
+            </div> */}
+          </section>
+          <footer>
+            <a href="https://bsky.app/profile/basammohamed.bsky.social">
+              <FaBluesky className="bluesky" />
+            </a>
+
+            <a href="https://github.com/BasamLive">
+              <FaGithub className="github" />
+            </a>
+            <a href="https://lichess.org/@/BasamRov">
+              <SiLichess className="lichess" />
+            </a>
+            <a href="https://x.com/mohamed_basam">
+              <FaXTwitter className="twitter" />
+            </a>
+            <a href="https://linkedin.com/in/basam249">
+              <FaLinkedin className="linkedin" />
+            </a>
+          </footer>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
